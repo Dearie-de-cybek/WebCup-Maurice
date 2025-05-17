cc3c29211bb7289a37746e69b3c9502564cdc3bd
Dearie <eburujacintha@gmail.com>
2025-05-17 21:09:02 +0400

    made a change here

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -7,7 +7,6 @@ import FeatureCard from './shared/FeatureCard';
 import FloatingElements from './shared/FloatingElements';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
-import Dashboard from './Dashboard';
 import { signUpUser, signInUser } from '../client';
 import Dashboard from '../pages/Dashboard';
 
