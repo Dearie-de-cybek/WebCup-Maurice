5877e0f25450a7243cc8f56d2ce37da1bab42bdd
Dearie <eburujacintha@gmail.com>
2025-05-17 13:06:57 +0400

    started the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import './index.css'
+/* eslint-disable no-unused-vars */
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import PageBuilder from "./pages/PageBuilder";
 
 function App() {
-  
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pagebuilder" element={<PageBuilder />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
